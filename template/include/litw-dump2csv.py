0fab7449a59bab0f9afc5779abb063a585548ba7
Nigini Oliveira <nigini@gmail.com>
2020-10-13 13:19:38 -0700

    Fixed bug with saving wanted tech and added data dump by entry.

--- template/include/litw-dump2csv.py
+++ template/include/litw-dump2csv.py
@@ -1,12 +1,10 @@
+import csv
+import json
+from pandas.io.json import json_normalize
+import argparse
 import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 
-import csv
-import json
-import pandas
-from pandas.io.json import json_normalize
-from datetime import datetime
-import argparse
 
 def clean_json_data(json_db_entry):
     clean_json = json_db_entry
@@ -53,11 +51,25 @@ def litw_data_by_UUID(litw_dump_data, data_types):
     return data_by_UUID
 
 
+def litw_data_by_entry(litw_dump_data, data_types):
+    data_by_line = []
+    for entry in litw_dump_data:
+        try:
+            data_type = entry.pop('data_type', None)
+            if data_type in data_types:
+                data_by_line.append(entry)
+        except:
+            print('Problem with this entry:')
+            print(entry)
+    return data_by_line
+
+
 def main():
     parser = argparse.ArgumentParser(description='Converts LITW data dumps containing JSON data to a CSV file formed by attributed per UUID.')
     parser.add_argument('input', help='the LITW data dump CSV file')
     parser.add_argument('output', help='the output file name')
     parser.add_argument('data_type', help='a comma separated list of data type to be written to the output file, e.g. litw:initialize,study:demographics,study:data')
+    parser.add_argument('--format', help='either TIDY (one entry per line) or FLAT (DEFAULT: aggregate all data by UUID - works only if all data is saved under a unique name.)')
     args = parser.parse_args()
     print(args)
 
@@ -66,8 +78,13 @@ def main():
     OUTPUT_FILE = args.output
 
     data = read_litw_data_dump(INPUT_FILE)
-    data_uuid = litw_data_by_UUID(data, DATA_TO_GET)
-    json_normalize(list(data_uuid.values())).to_csv(OUTPUT_FILE, index=False, quoting=csv.QUOTE_ALL)
+    if args.format == 'TIDY':
+        data_output = json_normalize(litw_data_by_entry(data, DATA_TO_GET))
+    else:
+        data_flat = litw_data_by_UUID(data, DATA_TO_GET)
+        data_output = json_normalize(list(data_flat.values()))
+
+    data_output.to_csv(OUTPUT_FILE, index=False, quoting=csv.QUOTE_ALL)
 
 
 if __name__ == "__main__":
